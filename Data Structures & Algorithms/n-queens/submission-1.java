5007d6ae2d066dec9b149e40749628f699b819df
ShockWave <sunnybharti3.142@gmail.com>
2026-06-18 13:55:11 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(0, board, result, n);
+        return result;
+    }
+
+    private void backtrack(int row, char[][] board, List<List<String>> result, int n) {
+        if (row == n){
+            result.add(construct(board));
+             return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1, board, result, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            // Same Column
+            if (board[i][col] == 'Q')
+                return false;
+        }
+        // Upar-Left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        // Upar-Right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    private List<String> construct(char[][] board){
+        List<String> path = new ArrayList<>();
+        for(int i = 0; i < board.length;i++){
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+}
