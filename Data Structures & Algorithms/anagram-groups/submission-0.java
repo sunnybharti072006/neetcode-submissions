ff8936f4179962926fb03233f853b37635e093ec
ShockWave <sunnybharti3.142@gmail.com>
2026-09-20 21:39:12 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s: strs){
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedKey = new String(charArray);
+
+            if(!map.containsKey(sortedKey)) map.put(sortedKey, new ArrayList<>());
+            map.get(sortedKey).add(s);
+           
+        }
+        return new ArrayList<>(map.values());
+    }
+}
