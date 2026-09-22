c7930519ca51a5c7e4a819c9a32ae3120b88c16c
ShockWave <sunnybharti3.142@gmail.com>
2026-09-22 20:56:56 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int trap(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int left = arr[i];
+            for (int j = 0; j < i; j++) {
+                left = Math.max(left, arr[j]);
+            }
+
+            int right = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                right = Math.max(right, arr[j]);
+            }
+            result += Math.min(left, right) - arr[i];
+        }
+        return result;
+    }
+}
