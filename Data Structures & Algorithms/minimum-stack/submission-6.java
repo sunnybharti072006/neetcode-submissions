79b327124f61f1764ee9732b9a016841894d2f4c
ShockWave <sunnybharti3.142@gmail.com>
2026-09-23 12:15:10 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,30 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minstack;
+    public MinStack() {
+        stack = new Stack<>();
+        minstack = new Stack<>();
+    }
+    
+    public void push(int val) {
+      stack.push(val);
+      if(minstack.isEmpty() || val <= minstack.peek()) minstack.push(val);  
+    }
+    
+    public void pop() {
+      if(!stack.isEmpty()){
+        int popped = stack.pop();
+        if(popped == minstack.peek()){
+            minstack.pop();
+        }
+      }  
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
