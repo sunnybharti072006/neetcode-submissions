4cf645fd243d2b9b7edb0561db584b4aa0ef6587
ShockWave <sunnybharti3.142@gmail.com>
2026-09-26 12:07:59 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        int currentTank = 0;
+        int startStation = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            currentTank += gas[i] - cost[i];
+
+            if (currentTank < 0) {
+                startStation = i + 1;
+                currentTank = 0;
+            }
+        }
+        return totalGas >= totalCost ? startStation : -1;
+    }
+}
